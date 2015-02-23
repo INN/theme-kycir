e5b1560a185f494ed7da1b0385ea0a5d46c1acc0
adam schweigert <aschweigert@gmail.com>
2015-02-23 14:33:09 -0500

    swap out mailchimp signup form

--- home.php
+++ home.php
@@ -113,9 +113,11 @@ $tags = of_get_option ('tag_display');
 		</div>
 		<div class="chevron span1"></div>
 		<div class="form span6">
-			<form action="http://kycir.us3.list-manage.com/subscribe/post?u=2020a220be1456f9d710d84cf&amp;id=901efefb0d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			<form action="//kycir.us7.list-manage.com/subscribe/post?u=0e9871c599e6afe1122a0a60d&amp;id=3fe1cce7ce" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
 				<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary">
+				<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				<div style="position: absolute; left: -5000px;"><input type="text" name="b_0e9871c599e6afe1122a0a60d_3fe1cce7ce" tabindex="-1" value=""></div>
 			</form>
 		</div>
 	</div>
